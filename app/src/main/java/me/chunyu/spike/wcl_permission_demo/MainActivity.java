c4e21e88a5f62a3fa1d57481117a988a303d8829
spike <morndragon@126.com>
2016-01-26 14:51:22 +0800

    完成

--- app/src/main/java/me/chunyu/spike/wcl_permission_demo/MainActivity.java
+++ app/src/main/java/me/chunyu/spike/wcl_permission_demo/MainActivity.java
@@ -1,6 +1,7 @@
 package me.chunyu.spike.wcl_permission_demo;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +12,17 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 0; // 请求码
+
+    // 所需的全部权限
     static final String[] PERMISSIONS = new String[]{
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.MODIFY_AUDIO_SETTINGS
     };
 
     @Bind(R.id.main_t_toolbar) Toolbar mTToolbar;
+
+    private PermissionsChecker mPermissionsChecker; // 权限检测器
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +32,27 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(mTToolbar);
 
-        PermissionsChecker checker = new PermissionsChecker(this);
+        mPermissionsChecker = new PermissionsChecker(this);
+    }
 
-        // 缺少权限提示
-        if (checker.lacksPermissions(PERMISSIONS)) {
-            Snackbar.make(mTToolbar, R.string.no_permissions, Snackbar.LENGTH_INDEFINITE).show();
+    @Override protected void onResume() {
+        super.onResume();
+
+        // 缺少权限时, 进入权限配置页面
+        if (mPermissionsChecker.lacksPermissions(PERMISSIONS)) {
+            startPermissionsActivity();
+        }
+    }
+
+    private void startPermissionsActivity() {
+        PermissionsActivity.startActivityForResult(this, REQUEST_CODE, PERMISSIONS);
+    }
+
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 拒绝时, 关闭页面, 缺少主要权限, 无法运行
+        if (requestCode == REQUEST_CODE && resultCode == PermissionsActivity.PERMISSIONS_DENIED) {
+            finish();
         }
     }
 }
